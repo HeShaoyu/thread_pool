15fc6fd5ae3e1d9e229bbb0eb8b92384017713a7
hsy <1530125120@qq.com>
2023-06-18 23:47:21 +0800

    20230618

--- course_code/CPP/05Array/arrayName.cpp
+++ course_code/CPP/05Array/arrayName.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-
+void printArr(char* a)
+{
+    strcpy(a, "world");
+}
 int main()
 {
     int a[5] = {1, 2, 3};
@@ -9,5 +12,14 @@ int main()
     std::cout << &a[0] << std::endl;
     std::cout << &a << std::endl;
     std::cout << a << std::endl;
+
+    int* b = (int*)malloc(sizeof(int) * 10);
+    b[0] = 10;
+    std::cout << b[0] << " " << b[1] << std::endl;
+
+    char c[10] = "hello";
+    std::cout << c << std::endl;
+    printArr(c);
+    std::cout << c << std::endl; 
     return 0;
 }
